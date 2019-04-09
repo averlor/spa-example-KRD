44c58d6ab9cf34ca8190725d663b2830baa024e5
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-09 17:23:36 +0300

    edit dependeces

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,79 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+      <v-navigation-drawer app temporary v-model="drawer">
+        <v-list>
+          <v-list-tile
+          v-for="item in menuItems" :key="item.id"
+          :to="item.route"
+          >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar app dark color="primary">
+          <v-toolbar-side-icon @click="drawer = !drawer" class="hidden-md-and-up">
+          </v-toolbar-side-icon>
+          <v-toolbar-title>Example App</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn v-for="item in menuItems" :key="item.id" flat :to="item.route">
+              <v-icon left>{{ item.icon }}</v-icon>
+              {{ item.title }}
+            </v-btn>
+          </v-toolbar-items>
+      </v-toolbar>
+      <v-content>
+          <router-view></router-view>
+      </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      menuItems: [
+        {
+          id: 1,
+          icon: 'lock',
+          title: 'Login',
+          route: '/signin'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+        },
+        {
+          id: 2,
+          icon: 'face',
+          title: 'Registration',
+          route: '/signout'
+        },
+        {
+          id: 3,
+          icon: 'bookmark_border',
+          title: 'Orders',
+          route: '/orders'
+        },
+        {
+          id: 4,
+          icon: 'note_add',
+          title: 'New ad',
+          route: '/new'
+        },
+        {
+          id: 5,
+          icon: 'list',
+          title: 'My ads',
+          route: '/list'
+        }
+      ]
+    }
+  }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,40 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+      drawer: false,
+      menuItems: [
+        {
+          id: 1,
+          icon: 'lock',
+          title: 'Login',
+          route: '/signin'
 
+        },
+        {
+          id: 2,
+          icon: 'face',
+          title: 'Registration',
+          route: '/signout'
+        },
+        {
+          id: 3,
+          icon: 'bookmark_border',
+          title: 'Orders',
+          route: '/orders'
+        },
+        {
+          id: 4,
+          icon: 'note_add',
+          title: 'New ad',
+          route: '/new'
+        },
+        {
+          id: 5,
+          icon: 'list',
+          title: 'My ads',
+          route: '/list'
+        }
+      ]
   },
   mutations: {
 
